dd8d7d9a6cfd254c000f786dd995c08989cbfa24
brandonleepiercy <b.piercy567@gmail.com>
2021-05-12 18:31:40 -0700

    created particle layer and the navbar, added homepage text

---
+++ src/components/homepage.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import Particles from 'react-particles-js';
+
+const particleParams = {
+    "particles": {
+        "color": {
+            "value": "#979799",
+        },
+        "number": {
+            "value": 150
+        },
+        "size": {
+            "value": 3,
+            "random":{
+                "enable": true,
+                "minimumValue": 1,
+            }
+        },
+        "shadow": {
+            "blur": 0,
+            "color": "#000000",
+            "enable": true
+        },
+        "value":10
+    },
+    "interactivity": {
+        "events": {
+            "onclick":{
+                "enable": true,
+                "mode": "push"
+            },
+            "onhover": {
+                "enable": true,
+                "mode": "repulse"
+            },
+            "modes":{
+                "repulse":{
+                    "distance": 20000,
+                    "duration": 0.4,
+                    "speed": 1
+                }
+            }
+        }
+    }
+}
+
+export default function Homepage(props){
+    return(
+        <div id='home-container'>
+            <Particles id='particle=layer' width='100vw' height='100vw' params={particleParams}></Particles>
+            <div className='home-content no-interaction'>
+                <h1 className='home-title'>Brandon Lee Piercy</h1>
+                <div className='subtitle-container'>
+                    <div className='subtitle'>Full Stack Developer / Javascript / React / SQL / MongoDB</div>
+                </div>
+            </div>
+        </div>
+    )
+} 
