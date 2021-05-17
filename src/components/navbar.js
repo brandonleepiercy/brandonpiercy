108b0b7704eb4a16b3c17fe66ed41cd96a02b203
Brandon Piercy <b.piercy567@gmail.com>
2021-05-16 19:48:47 -0700

    optimized better for laptops and added nav hide based on scroll position

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import resume from '../resume/Resume.pdf';
 
+let preScroll = window.pageYOffset;
+
+window.onscroll = function() {
+  let postScroll = window.pageYOffset;
+  if (preScroll > postScroll) {
+    document.getElementById("navbar-container").style.top = "0";
+  } else {
+    document.getElementById("navbar-container").style.top = "-5em";
+  }
+  preScroll = postScroll;
+}
+
 export default class NavBar extends React.Component {
     handleClick = (event) => {
         var sectionTitle = event.target.innerText;
