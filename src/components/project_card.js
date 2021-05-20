43a751b9cf6f6747488697be0c2b2a781cf69370
brandonleepiercy <b.piercy567@gmail.com>
2021-05-20 13:22:45 -0700

    further changes to project card styles

--- src/components/project_card.js
+++ src/components/project_card.js
@@ -4,25 +4,27 @@ import {AiOutlineRocket, AiFillGithub, AiFillFolderOpen} from 'react-icons/ai'
 export default function ProjectCard(props) {
     if (props.deployLink !== null) {
         return (
-            <div className='project-card' style={{backgroundImage: `url(${props.picture})`}}>
+            <div className='project-card' style={{background: `-webkit-linear-gradient(rgba(29, 38, 113, 0.8), rgba(195, 55, 100, 0.8)), url(${props.picture})`}}>
                 <div className='project-card-body'>
-                    <div className='project-icon-row'>
-                        <div className='folder-icon-container'>
-                            <AiFillFolderOpen className='project-folder-icon'/>
+                    <div className='project-card-overlay'>
+                        <div className='project-icon-row'>
+                            <div className='folder-icon-container'>
+                                <AiFillFolderOpen className='project-folder-icon'/>
+                            </div>
+                            <div className='project-icon-container'>
+                                <a href={props.deployLink} target='_blank' rel='noopener noreferrer'>
+                                    <AiOutlineRocket className='project-link-icon'/>
+                                </a>
+                                <a href={props.githubLink} target='_blank' rel='noopener noreferrer'>
+                                    <AiFillGithub className='project-link-icon'/>
+                                </a>
+                            </div>
                         </div>
-                        <div className='project-icon-container'>
-                            <a href={props.deployLink} target='_blank' rel='noopener noreferrer'>
-                                <AiOutlineRocket className='project-link-icon'/>
-                            </a>
-                            <a href={props.githubLink} target='_blank' rel='noopener noreferrer'>
-                                <AiFillGithub className='project-link-icon'/>
-                            </a>
+                        <div className='project-info-row'>
+                            <div className='project-title'>{props.name}</div>
+                            <div className='project-title-desc-border'/>
+                            <div className='project-desc'>{props.description}</div>
                         </div>
-                    </div>
-                    <div className='project-info-row'>
-                        <div className='project-title'>{props.name}</div>
-                        <div className='project-title-desc-border'/>
-                        <div className='project-desc'>{props.description}</div>
                     </div>
                 </div>
             </div>
@@ -31,20 +33,22 @@ export default function ProjectCard(props) {
         return (
             <div className='project-card' style={{backgroundImage: `url(${props.picture})`}}>
                 <div className='project-card-body'>
-                    <div className='project-icon-row'>
-                        <div className='folder-icon-container'>
-                            <AiFillFolderOpen className='project-folder-icon'/>
+                    <div className='project-card-overlay'>
+                        <div className='project-icon-row'>
+                            <div className='folder-icon-container'>
+                                <AiFillFolderOpen className='project-folder-icon'/>
+                            </div>
+                            <div className='project-icon-container'>
+                                <a href={props.githubLink} target='_blank' rel='noopener noreferrer'>
+                                    <AiFillGithub className='project-link-icon'/>
+                                </a>
+                            </div>
                         </div>
-                        <div className='project-icon-container'>
-                            <a href={props.githubLink} target='_blank' rel='noopener noreferrer'>
-                                <AiFillGithub className='project-link-icon'/>
-                            </a>
+                        <div className='project-info-row'>
+                            <div className='project-title'>{props.name}</div>
+                            <div className='project-title-desc-border'/>
+                            <div className='project-desc'>{props.description}</div>
                         </div>
-                    </div>
-                    <div className='project-info-row'>
-                        <div className='project-title'>{props.name}</div>
-                        <div className='project-title-desc-border'/>
-                        <div className='project-desc'>{props.description}</div>
                     </div>
                 </div>
             </div>
